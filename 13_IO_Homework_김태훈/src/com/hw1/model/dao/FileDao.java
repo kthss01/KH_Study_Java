243a43822f88253ab8ee4949c34eaab234c1933b
kthss01 <kthss01@naver.com>
2021-05-26 15:36:30 +0900

    17일차 과제 수정

--- 13_IO_Homework_김태훈/src/com/hw1/model/dao/FileDao.java
+++ 13_IO_Homework_김태훈/src/com/hw1/model/dao/FileDao.java
@@ -142,43 +142,4 @@ public class FileDao {
 		}
 	}
 
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		FileDao fd = new FileDao();
-
-		// 반복문을 통한 메뉴 실행
-		while (true) {
-			System.out.println("****** MyNote ******");
-
-			System.out.println("1. 노트 새로 만들기");
-			System.out.println("2. 노트 열기");
-			System.out.println("3. 노트 열어서 수정하기");
-			System.out.println("4. 끝내기");
-
-			System.out.print("번호를 입력하세요 : ");
-			int select = sc.nextInt();
-			sc.nextLine();
-
-			switch (select) {
-			case 1:
-				fd.fileSave();
-				break;
-			case 2:
-				fd.fileOpen();
-				break;
-			case 3:
-				fd.fileEdit();
-				break;
-			case 4:
-				System.out.println("프로그램을 종료합니다.");
-				return;
-			default:
-				System.out.println("잘못 입력하셨습니다.");
-				break;
-			}
-
-		}
-	}
-
 }
