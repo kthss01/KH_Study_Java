35a9ab6b738cacbaa3802030446f4054910bc47e
kthss01 <kthss01@naver.com>
2021-05-27 22:56:18 +0900

    19일차 수업

--- 14_Collection_Homework_김태훈/src/com/kh/collection/silsub2/controller/BookManager.java
+++ 14_Collection_Homework_김태훈/src/com/kh/collection/silsub2/controller/BookManager.java
@@ -45,13 +45,13 @@ public class BookManager {
 		ArrayList<Book> books = bd.sortedBookList();
 
 //		return (Book[]) books.toArray(); // 이렇게 해버리면 문제 발생한다고 함  ClassCastException
-		// 사유 
+		// 사유
 		// Object가 부모 클래스이고 자식이 String 클래스인데 상위 클래스에서 하위 클래스로 캐스팅이 되지 않기 때문에 발생하는 문제라고함
 		// int double은 자료형 변환이라 가능하지만 클래스이 캐스팅에선 안됨
-		
-		return books.toArray(new Book[books.size()]); // 이와 같은 방법으로  해결 가능
+
+		return books.toArray(new Book[books.size()]); // 이와 같은 방법으로 해결 가능
 //		return books.stream().toArray(Book[]::new); // 이 방법도 있긴함
-		
+
 	}
 
 	public void printBookList(Book[] br) {
